d4fa743f98cbcac6dec0f9c4132a0e89d4940731
maungmatubbar <maungmatubbar@gmail.com>
2021-05-06 21:05:51 +0600

    Insatall Vue laravel 8,Node js install,Vuetify 1.5.24 intall,vue router and use initial components

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <toolbar></toolbar>
+      <AppFooter></AppFooter>
+  </div>
+</template>
+
+<script>
+import toolbar from './Toolbar'
+import AppFooter from "./AppFooter";
+    export default {
+        components:{
+            toolbar,AppFooter
+        }
+    }
+
+</script>
+<style>
+
+</style>
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-toolbar>
+<!--        <v-toolbar-side-icon></v-toolbar-side-icon>-->
+        <v-toolbar-title>RealApp</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>FORUM</v-btn>
+            <v-btn flat>ASK QUESTION</v-btn>
+            <v-btn flat>CATEGORY</v-btn>
+            <v-btn flat>LOGIN</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
